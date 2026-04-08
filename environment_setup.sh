f302ce2a68513539de331ef41ece1cb73ce0daa1
Sanyam Mehta <sanyammehtachd@gmail.com>
2026-04-08 12:30:21 -0400

    Removing conda env installation command; Gronding DINO gets installed from Chahyon Ku's repo

--- environment_setup.sh
+++ environment_setup.sh
@@ -5,10 +5,6 @@
 # Set CUDA_HOME
 export CUDA_HOME=$CONDA_PREFIX
 
-# install requirements
-# pip install -r requirements.txt
-conda env create -f environment.yml
-
 # Set cmake prefix path for installing sophus
 SITE_PACKAGES_DIR=$(python -c "import site; print(site.getsitepackages()[0])")
 PYBIND_11_DIR="$SITE_PACKAGES_DIR/pybind11/share/cmake/pybind11"
@@ -32,7 +28,7 @@ pip install --no-deps webdataset===0.1.40 faster-fifo open3d==0.19.0 sophuspy===
 rm -rf GroundingDINO
 
 # Clone GroundingDINO
-git clone https://github.com/IDEA-Research/GroundingDINO.git
+git clone https://github.com/chahyon-ku/grounding-dino GroundingDINO
 cd GroundingDINO
 pip install -e .
 
